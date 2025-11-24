84fb9a78f4376db90d769d2bef431ea9bbfa6adb
Toba <tobaodumosu1@gmail.com>
2025-11-24 15:43:03 +0000

    fix: added delete if statement for new data

--- API/app/crud.py
+++ API/app/crud.py
@@ -2,8 +2,11 @@ from sqlalchemy.orm import Session
 import models, schemas
 from models import create_dynamic_localmetadata
 
-def create_localmetadata(db: Session, localmetadata: schemas.LocalMetadataCreate):
+def create_localmetadata(db: Session, GameID: str, localmetadata: schemas.LocalMetadataCreate):
     from main import LocalMetadataModel
+    check_row = db.query(LocalMetadataModel).filter(LocalMetadataModel.GameID == GameID).first()
+    if check_row:
+        db.delete(check_row)
     db_localmetadata = LocalMetadataModel(GameID=localmetadata.GameID, GameName=localmetadata.GameName, LastModified=localmetadata.LastModified, DeviceID=localmetadata.DeviceID)
     db.add(db_localmetadata)
     db.commit()
