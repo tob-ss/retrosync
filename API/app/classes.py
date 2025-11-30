aaba14e24b6c401b81d7f325c0541e1bff4195a5
Toba <tobaodumosu1@gmail.com>
2025-11-30 23:49:46 +0000

    fix: testing new logic for dealing with cloud dupelicates

--- API/app/classes.py
+++ API/app/classes.py
@@ -31,8 +31,8 @@ class DupeCloudMDRemover:
 
     def get_gamesby_LID(self):
         from main import MetadataModel
-        LID_table = self.db.query(MetadataModel).filter(MetadataModel.LID == self.LID).first()
+        LID_table = self.db.query(MetadataModel).filter(MetadataModel.LID == self.LID)
         print(LID_table)
-        return LID_table
+        #return LID_table
     #def delete_dupe_games(self):
     #    print(self.get_gamesby_LID())
