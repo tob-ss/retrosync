ce39886151c928403c19da5db0fa3a7c45ac283f
Toba <tobaodumosu1@gmail.com>
2025-12-01 16:09:06 +0000

    fix: shifted logic from crud to classes

--- API/app/classes.py
+++ API/app/classes.py
@@ -26,9 +26,9 @@ class LocalMetadataFlusher:
 
     def flush_metadata(self):
         from main import MetadataModel
-        print(self.DeviceID)
+        print(f"{MetadataModel.LID} and {MetadataModel.DeviceID}")
         db_localmetadata = self.db.query(MetadataModel).filter(MetadataModel.LID == "L", MetadataModel.DeviceID == self.DeviceID).all()
-        
+        print("i didn't get here successfully")
         for x in db_localmetadata:
             if x.LID == "L" and x.DeviceID == self.DeviceID:
                 self.db.delete(x)
