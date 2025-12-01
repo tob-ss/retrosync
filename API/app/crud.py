9cb8b52fe2887857efeeab8bf3578041d7a9cd67
Toba <tobaodumosu1@gmail.com>
2025-12-01 15:24:33 +0000

    fix: fixed circular import

--- API/app/crud.py
+++ API/app/crud.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
 import models, schemas
-from main import MetadataModel
+
 
 def create_metadata(db: Session, metadata: schemas.MetadataCreate):
+    from main import MetadataModel
     db_metadata = MetadataModel(LID=metadata.LID, GameID=metadata.GameID, GameName=metadata.GameName, LastModified=metadata.LastModified, DeviceID=metadata.DeviceID, Cloud=metadata.Cloud)
     db.add(db_metadata)
     db.commit()
@@ -11,6 +12,7 @@ def create_metadata(db: Session, metadata: schemas.MetadataCreate):
     return db_metadata
 
 def flush_localmetadata(db: Session, DeviceID: str):
+    from main import MetadataModel
     db_localmetadata = db.query(MetadataModel).filter(MetadataModel.LID == "L", MetadataModel.DeviceID == DeviceID)
     if db_localmetadata:
         db.delete(db_localmetadata)
@@ -19,6 +21,7 @@ def flush_localmetadata(db: Session, DeviceID: str):
     return None
 
 def create_metadata_cloud(db: Session, metadata: schemas.MetadataCreate):
+    from main import MetadataModel
     db_metadata = MetadataModel(LID=metadata.LID, GameID=metadata.GameID, GameName=metadata.GameName, LastModified=metadata.LastModified, DeviceID=metadata.DeviceID, Cloud=metadata.Cloud)
     db.add(db_metadata)
     db.commit()
