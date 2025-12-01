6fdd9bfa7e65c60ad055a2049292d01eaaeefb40
Toba <tobaodumosu1@gmail.com>
2025-12-01 22:35:37 +0000

    fix: shifted logic from crud to classes

--- API/app/main.py
+++ API/app/main.py
@@ -31,7 +31,7 @@ def create_metadata(metadata: schemas.MetadataCreate, db: Session = Depends(get_
     append_LMD = LMP(db, metadata)
     return append_LMD.append_metadata()
 
-@app.post("/metadata/delete/localflush/", response_model=schemas.Metadata)
+@app.post("/metadata/delete/localflush/", response_model=schemas.Metadata_Device)
 def flush_localmetadata(DeviceID: schemas.Metadata_Device, db: Session = Depends(get_db)):
     try:
         print(DeviceID)
