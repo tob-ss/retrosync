a585a376832d4153a6d2e875e2bb071bf8ee773a
Toba <tobaodumosu1@gmail.com>
2025-12-01 10:49:00 +0000

    fix: testing new logic for dealing with cloud duplicates

--- API/app/classes.py
+++ API/app/classes.py
@@ -37,6 +37,7 @@ class DupeCloudMDRemover:
         LID_table = self.db.query(MetadataModel).filter(MetadataModel.LID == self.LID).all()
         for x in LID_table:
             duplicate_row = self.db.query(MetadataModel).filter(x.GameID == self.GameID, x.LastModified == self.LastModified).first()
+            print(f"current row game id is{x.GameID} and the gameid that has been passed in is {self.GameID}")
             print(duplicate_row)
             if duplicate_row:
                 print("I would delete a dupe record now!")
