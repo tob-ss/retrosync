9e20a42e55578e7b8c831a134693c2fbd05c80e1
Toba <tobaodumosu1@gmail.com>
2025-12-01 09:32:38 +0000

    fix: testing new logic for dealing with cloud dupelicates

--- API/app/classes.py
+++ API/app/classes.py
@@ -4,6 +4,7 @@ from database import engine, Base, get_db
 from models import create_dynamic_metadata, create_dynamic_syncrequests
 import models, schemas, crud
 from sqlalchemy.orm import Session
+from decimal import Decimal
 
 class LocalMetadataProcessor:
     def __init__(self, db: Session, localmetadata: schemas.MetadataCreate):
@@ -25,15 +26,23 @@ class SyncRequestProcessor:
         return crud.create_syncrequest(self.db, self.syncrequest)
     
 class DupeCloudMDRemover:
-    def __init__(self, db: Session, LID: str):
+    def __init__(self, db: Session, LID: str, GameID: str, LastModified: Decimal):
         self.db = db
         self.LID = LID
+        self.GameID = GameID
+        self.LastModified = LastModified
 
     def get_gamesby_LID(self):
         from main import MetadataModel
         LID_table = self.db.query(MetadataModel).filter(MetadataModel.LID == self.LID).all()
         for x in LID_table:
-            self.db.delete(x)
+            duplicate_row = self.db.query(x).filter(x.GameID == self.GameID, x.LastModified == self.LastModified).first()
+            if duplicate_row:
+                print("I would delete a dupe record now!")
+                self.db.delete(duplicate_row)
+            else:
+                continue
+            #for x in lid table, if self.db.query(x).filter(x.gameid == self.gameid, x.lastmod == self.lastmod). maybe .first()? if not do a second loop with .all()
             self.db.commit()
         return LID_table
         #return LID_table
