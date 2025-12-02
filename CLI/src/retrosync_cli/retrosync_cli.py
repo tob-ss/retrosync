7bac4f459332dac5ce65c8044f5ee54e6314d341
Toba <tobaodumosu1@gmail.com>
2025-12-02 12:15:14 +0000

    feat: moved logic for checking daemon status in cli to class

--- CLI/src/retrosync_cli/retrosync_cli.py
+++ CLI/src/retrosync_cli/retrosync_cli.py
@@ -2,6 +2,24 @@ import click
 import requests
 import time
 
+class DaemonStatusChecker:
+    def __init__(self, DeviceID: str):
+        self.DeviceID = DeviceID
+        
+    def check_status(self):
+        get_url = "http://37.27.217.84//daemon/status/"
+        x = {"DeviceID": self.DeviceID}
+        get_status = requests.get(get_url, params=x)
+        return get_status.text
+    
+    def continue_or_cancel(self):
+        response = self.check_status()
+        print(response)
+        if response == f"{self.DeviceID} is Online":
+            return 1
+        if response == f"{self.DeviceID} is Offline":
+            return 0
+
 @click.group()
 def cli():
     """Retrosync's CLI for managing your Retro Game Saves!"""
@@ -14,36 +32,34 @@ def cli():
 def upload(device, game, all):
     """Choose which game saves you would like to upload!"""
     sync_url = "http://37.27.217.84//sync/append/"
-    get_url = "http://37.27.217.84//daemon/status/"
+    
     if all and game:
         raise click.BadParameter((click.style("To upload all game saves, please omit the --game or -g flag.", fg='red')))
     if all == False:
         if click.confirm(f"Please confirm that you would like to upload {game} game save from the following device: {device}"):
             click.echo(f"Checking if device {device} is online...")
-            x = {"DeviceID": device}
-            try:
-                get_status = requests.get(get_url, params=x, timeout=(3,10))
-                print(get_status)
-            except requests.Timeout:
-                print("The request timed out")
-
-            click.echo("Upload Started!")
-            n = {"DeviceID": device, "Operation": "Upload", "GameID": game, "Completed": False, "TimeStamp": int(time.time())}
-            print(n)
-            sync_request = requests.post(sync_url, json = n)
+            status = DaemonStatusChecker(device)
+            if status.continue_or_cancel() == 1:
+                click.echo("Upload Started!")
+                n = {"DeviceID": device, "Operation": "Upload", "GameID": game, "Completed": False, "TimeStamp": int(time.time())}
+                print(n)
+                sync_request = requests.post(sync_url, json = n)
+            else:
+                click.echo(f"Could not connect to {device}... Please check if Retrosync is running or if device is connected to the internet")
+                click.echo(f"Cancelling Upload...")   
     else:
         if click.confirm(f"Please confirm that you would like to upload all game saves from the following device: {device}"):
             click.echo(f"Checking if device {device} is online...")
-            x = {"DeviceID": device}
-            #try:
-            get_status = requests.get(get_url, params=x)
-            click.echo(get_status.text)
-            #except requests.Timeout:
-            #    print("The request timed out")
-            click.echo("Upload Started!")
-            n = {"DeviceID": device, "Operation": "Upload", "GameID": "ALL", "Completed": False, "TimeStamp": int(time.time())}
-            print(n)
-            sync_request = requests.post(sync_url, json = n)
+            status = DaemonStatusChecker(device)
+            if status.continue_or_cancel() == 1:
+                click.echo(f"{device} is online... Starting Upload!")
+                click.echo("Upload Started!")
+                n = {"DeviceID": device, "Operation": "Upload", "GameID": "ALL", "Completed": False, "TimeStamp": int(time.time())}
+                print(n)
+                sync_request = requests.post(sync_url, json = n)
+            else:
+                click.echo(f"Could not connect to {device}... Please check if Retrosync is running or if device is connected to the internet")
+                click.echo(f"Cancelling Upload...")
 
 @cli.command()
 @click.option('--device','-d', help='Name of the device you want to download saves to.')
