76be82c563fd66c41954b14b2393711e0163d836
Toba <tobaodumosu1@gmail.com>
2025-12-08 17:46:03 +0000

    fix: added logic to wait for request to complete

--- Frontend/retrosync-preprod/src/components/PostRequest.js
+++ Frontend/retrosync-preprod/src/components/PostRequest.js
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
 import React, { useState } from 'react';
+import { useRef } from 'react';
 import checkStatus  from '../services/DaemonStatusChecker';
 
 const SubmitSync = () => {
