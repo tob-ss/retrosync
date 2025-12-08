8a169781d8d7d0175fa1b1b8ef9d5a30fa6948be
Toba <tobaodumosu1@gmail.com>
2025-12-08 11:44:46 +0000

    feat: added some testing files to check react page

---
+++ Frontend/retrosync-preprod/components/test_app.js
@@ -0,0 +1,10 @@
+import MyButton from './test_button.js';
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton />
+        </div>
+    );
+}
