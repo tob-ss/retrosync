99c7f403dc312698b5a1c1deb0cc8ceefc2fd5e9
Toba <tobaodumosu1@gmail.com>
2025-12-08 16:30:41 +0000

    fix: changed get url

--- Frontend/retrosync-preprod/src/services/DaemonStatusChecker.js
+++ Frontend/retrosync-preprod/src/services/DaemonStatusChecker.js
@@ -2,7 +2,7 @@ const checkStatus = (DeviceID) => {
     const get_url = "http://37.27.217.84/daemon/status/"
     const params = new URLSearchParams();
     params.append("DeviceID", DeviceID);
-    const response = fetch(`http://37.27.217.84/daemon/status/${params}`);
+    const response = fetch(`http://37.27.217.84/daemon/status?${params}`);
     if (response === "1") {
         return 1
     }
