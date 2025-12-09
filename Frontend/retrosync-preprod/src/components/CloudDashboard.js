3a8b99e3a3eef10526161eee22a58ca6793c7163
Toba <tobaodumosu1@gmail.com>
2025-12-09 14:53:51 +0000

    feat: added placeholder box arts to placeholder gallery

--- Frontend/retrosync-preprod/src/components/CloudDashboard.js
+++ Frontend/retrosync-preprod/src/components/CloudDashboard.js
@@ -3,8 +3,8 @@ import "react-photo-album/rows.css";
 
 function cloudDashboard() {
     const photos = [
-        { src: "https://art.gametdb.com/wii/cover3D/EN/RMCP01.png?1528403592" },
-        { src: "https://art.gametdb.com/wii/cover3D/US/SMNE01.png?1317736150" },
+        { src: "https://art.gametdb.com/wii/cover3D/EN/RMCP01.png?1528403592",  width: 176, height: 248 },
+        { src: "https://art.gametdb.com/wii/cover3D/US/SMNE01.png?1317736150",  width: 176, height: 248 },
     ];
 
    return <RowsPhotoAlbum photos={photos} />
